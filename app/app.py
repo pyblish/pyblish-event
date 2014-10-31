0654b6f9ab45f7df228507a37066150f64fe46da
Marcus Ottosson <konstruktion@gmail.com>
2014-10-31 17:07:51 +0000

    Basic REST implemented

--- app/app.py
+++ app/app.py
@@ -1,7 +1,5 @@
 # Standard library
 import os
-import time
-import threading
 
 # Dependencies
 import flask
@@ -10,7 +8,7 @@ import flask.ext.socketio
 
 # Local library
 import com
-# import restful
+import restful
 import routes.home
 
 
@@ -21,41 +19,23 @@ app.route("/<path:p>")(routes.home.route)  # All paths route to index.html
 
 
 # Socket.io
-_socketio = flask.ext.socketio.SocketIO(app)
-_socketio.on("connect", namespace="/default")(com.connect)
+socket = flask.ext.socketio.SocketIO(app)
+socket.on("connect", namespace="/default")(com.connect)
 
 
-# RESTful API
-# _api = flask.ext.restful.Api(app)
-# _api.add_resource(restful.api(socket=_socketio, data={}), "/api")
-
-
-def background_push():
-    while True:
-        print "Pushing.."
-        time.sleep(2)
-        _socketio.emit("event", {
-            "instance": "From Flask",
-            "family": "napoleon.test",
-            "comment": "Flask comment",
-            "author": "Maaaarcus"},
-            namespace="/default")
-
-
-print "Starting thread.."
-thread = threading.Thread(target=background_push)
-thread.daemon = True
-thread.start()
+# RESTful
+api = flask.ext.restful.Api(app)
+api.add_resource(restful.api(socket=socket), "/api")
 
 
 def debug(app):
-    os.environ["DEVELOP"] = "true"
+    # os.environ["DEVELOP"] = "true"
     app.debug = True
-    return _socketio.run(app)
+    return socket.run(app)
 
 
 def production(app):
-    return _socketio.run(app)
+    return socket.run(app)
 
 
 if __name__ == "__main__":
--- app/restful.py
+++ app/restful.py
@@ -1,12 +1,29 @@
+import json
 import flask.ext.restful
+import uuid
+
+import model
 
 
-def api(socket, data):
+def api(socket):
     class Api(flask.ext.restful.Resource):
         def get(self):
-            return data
+            return model.data
 
         def post(self):
-            pass
+            unique_id = str(uuid.uuid4())
+
+            event = flask.request.stream.read()
+            event_json = json.loads(event)
+            event_json['id'] = unique_id
+
+            # model.data.append(event_json)
+
+            print "Emitting %s" % event_json
+            socket.emit("event",
+                        event_json,
+                        namespace="/default")
+
+            return unique_id
 
     return Api
